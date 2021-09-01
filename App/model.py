5800ba69529e5f6c8647b90ae0be066b19759aed
JSofia01 <j.ahumadaa@uniandes.edu.co>
2021-09-01 16:40:50 -0500

    Primera entrega - Reto 1

--- App/model.py
+++ App/model.py
@@ -37,9 +37,87 @@ los mismos.
 
 # Construccion de modelos
 
+def newCatalog():
+
+    catalog = {'artist': None,
+               'artworks': None,}
+
+    catalog['artist'] = lt.newList('SINGLE_LINKED')
+    catalog['artworks'] = lt.newList('SINGLE_LINKED')
+
+    return catalog
+
 # Funciones para agregar informacion al catalogo
 
+def addArtist (catalog, artist):
+    art = newArtist(artist['artist_Id'], artist['artist_Name'],
+                    artist['artist_Bio'], artist['artist_Nacionality'],
+                    artist['artist_Gender'], artist['artist_BeginDate'],
+                    artist['artist_EndDate'], artist['artist_WikiQID'], artist['artist_ULAN'])
+    lt.addLast(catalog['artist'], art)
+
+def addArtworks (catalog, artworks):
+    artw = newArtwork(artworks['artworks_ObjectID'], artworks['artworks_Title'], artworks['artworks_ConstituentID'],
+                    artworks['artworks_Date'], artworks['artworks_Medium'], artworks['artworks_Dimensions'],
+                    artworks['artworks_CreditLine'], artworks['artworks_AccessionNumber'], artworks['artworks_Classification'],
+                    artworks['artworks_Department'], artworks['artworks_DateAcquired'], artworks['artworks_Cataloged'],
+                    artworks['artworks_URL'], artworks['artworks_Circumference (cm)'], artworks['artworks_Depth (cm)'],
+                    artworks['artworks_Diameter (cm)'], artworks['artworks_Height (cm)'], artworks['artworks_Length (cm)'],
+                    artworks['artworks_Weight (kg)'], artworks['artworks_Width (cm)'], artworks['artworks_Seat Height (cm)'],
+                    artworks['artworks_Duration (sec.)'])
+    lt.addLast(catalog['artworks'], artw)			
+
 # Funciones para creacion de datos
+
+def newArtist(Id, Name, Bio, Nacionality, Gender, BeginDate, EndDate, WikiQID, ULAN):
+    artist = {'Id': '', 'Name': '', 'Bio': '',
+            'Nacionality': '', 'Gender': '', 'BeginDate': '',
+            'EndDate': '', 'WikiQID': '', 'ULAN': ''}
+    artist['artist_Id'] = Id
+    artist['artist_Name'] = Name
+    artist['artist_Bio'] = Bio
+    artist['artist_Nacionality'] = Nacionality
+    artist['artist_Gender'] = Gender
+    artist['artist_BeginDate'] = BeginDate
+    artist['artist_EndDate'] = EndDate
+    artist['artist_WikiQID'] = WikiQID
+    artist['artist_ULAN'] = ULAN
+    
+    return artist
+
+def newArtwork(ObjectID, Title, ConstituentID, Date, Medium, Dimensions, CreditLine, AccessionNumber, Classification,
+            Department, DateAcquired, Cataloged, URL, Circumference, Depth, Diameter, Height, Length, Weight, Width,
+            SeatHeight, Duration):
+
+    artworks = {'ObjectID': '', 'Title': '', 'ConstituentID': '', 'Date': '',
+                'Medium': '', 'Dimensions': '', 'CreditLine': '', 'AccessionNumber': '',
+                'Classification': '', 'Department': '', 'DateAcquired': '', 'Cataloged': '',
+                'URL': '', 'Circumference': '', 'Depth': '', 'Diameter': '', 'Height': '',
+                'Length': '', 'Weight': '', 'Width': '', 'SeatHeight': '', 'Duration': ''}
+    artworks['artworks_ObjectID'] = ObjectID
+    artworks['artworks_Title'] = Title
+    artworks['artworks_ConstituentID'] = ConstituentID
+    artworks['artworks_Date'] = Date
+    artworks['artworks_Medium'] = Medium
+    artworks['artworks_Dimensions'] = Dimensions
+    artworks['artworks_CreditLine'] = CreditLine
+    artworks['artworks_AccessionNumber'] = AccessionNumber
+    artworks['artworks_Classification'] = Classification
+    artworks['artworks_Department'] = Department
+    artworks['artworks_DateAcquired'] = DateAcquired
+    artworks['artworks_Cataloged'] = Cataloged
+    artworks['artworks_URL'] = URL
+    artworks['artworks_Circumference'] =  Circumference
+    artworks['artworks_Depth'] = Depth
+    artworks['artworks_Diameter'] = Diameter
+    artworks['artworks_Height'] = Height
+    artworks['artworks_Length'] = Length 
+    artworks['artworks_Weight'] = Weight
+    artworks['artworks_Width'] = Width
+    artworks['artworks_Seat Height'] = SeatHeight
+    artworks['artworks_Duration'] = Duration
+    
+    return artworks
 
 # Funciones de consulta
 
